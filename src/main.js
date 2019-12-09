e785c29d644401959d8b4027af9ab854676822f3
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-12-09 20:30:16 +0700

    added bootstrap, jquery, popperjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+import "popper.js";
+import "./assets/app.scss";
 
 Vue.config.productionTip = false;
 
